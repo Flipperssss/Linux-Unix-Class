8b3abcc8678e34ca2752308247bd5a23aeac8214
frrobledo <frrobledo@tcu.edu>
2025-10-21 16:51:08 -0500

    Finalized daily_task.sh integration script

--- daily_task.sh
+++ daily_task.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+# Run lab_tools.sh
 ./lab_tools.sh
 
+# Run system_report.sh
 ./system_report.sh
 
 echo "Task run by $USER on $(date)" >> summary.txt
+
+echo "Daily automation complete. Report saved."
+
+cat summary.txt
 
