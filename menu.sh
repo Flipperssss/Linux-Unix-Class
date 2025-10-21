a1a3a8974b4e70d2c3518f36d74d0e31ea66d091
frrobledo <frrobledo@tcu.edu>
2025-10-21 15:23:47 -0500

    Implemented case handling in menu.sh

--- menu.sh
+++ menu.sh
@@ -6,5 +6,22 @@ echo "3) Show logged-in users"
 echo "4) Exit"
 
 read -p "Enter your choice [1-4]: " choice
-echo "You selected: $choice"
+
+case $choice in
+    1)
+        date
+        ;;
+    2)
+        pwd
+        ;;
+    3)
+        who
+        ;;
+    4)
+        echo "Exiting..."
+        ;;
+    *)
+        echo "Invalid choice"
+        ;;
+esac
 
