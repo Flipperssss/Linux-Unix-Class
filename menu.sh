8516c4a0f441c3c0857478e8bcb4dca63da4d557
frrobledo <frrobledo@tcu.edu>
2025-10-21 15:15:35 -0500

    Added menu display and input handling to menu.sh

---
+++ menu.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "1) Show current date"
+echo "2) Show current directory"
+echo "3) Show logged-in users"
+echo "4) Exit"
+
+read -p "Enter your choice [1-4]: " choice
+echo "You selected: $choice"
+
