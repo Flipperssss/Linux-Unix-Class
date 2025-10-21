4254a15f1d8733141940fdcb635e7fd26bf5d1fd
frrobledo <frrobledo@tcu.edu>
2025-10-21 16:13:42 -0500

    Added show user function to lab_tools.sh

--- lab_tools.sh
+++ lab_tools.sh
@@ -4,6 +4,12 @@ show_date() {
     echo "Current date and time: $(date)"
 }
 
-# Test function
+show_user() {
+    echo "Username: $USER"
+    echo "Hostname: $HOSTNAME"
+}
+
+# Test functions
 show_date
+show_user
 
