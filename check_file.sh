b48bcee8eba8ddefe482a22c48af9feda5ed4f71
frrobledo <frrobledo@tcu.edu>
2025-10-21 14:53:49 -0500

    Added file creation option to check_file.sh

--- check_file.sh
+++ check_file.sh
@@ -13,5 +13,12 @@ then
     echo "File '$filename' exists."
 else
     echo "File '$filename' not found."
+    read -p "Do you want to create it? (y/n): " choice
+    if [ "$choice" = "y" ]
+    then
+        touch "$filename"
+        echo "File '$filename' created."
+    else
+        echo "Exiting without creating file."
+    fi
 fi
-
